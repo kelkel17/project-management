f2d27d2311744ac51196d381370fb4682654fe49
Mickale Saturre <saturre.mic2@gmail.com>
2019-05-31 12:44:47 +0800

    Create Project Summary Component (static data)

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,15 +1,12 @@
 import React from 'react';
 
+// Components
+import ProjectSummary from './ProjectSummary'
+
 const ProjectList = (props) => {
   return (
     <div className="project-list section">
-    	<div className="project-summary card z-depth-0">
-    		<div className="card-content grey-text text-darken-3">
-    			<span className="card-title">Project Title</span>
-    			<p>Posted by Micmic</p>
-    			<p className="grey-text">31st of May, 12pm</p>
-    		</div>
-    	</div>
+    	<ProjectSummary />
     </div>
   )
 }
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProjectSummary = (props) => {
+	return (
+		<div className="project-summary card z-depth-0">
+  		<div className="card-content grey-text text-darken-3">
+  			<span className="card-title">Project Title</span>
+  			<p>Posted by Micmic</p>
+  			<p className="grey-text">31st of May, 12pm</p>
+  		</div>
+  	</div>
+	)
+}
+
+export default ProjectSummary

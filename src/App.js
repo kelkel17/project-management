d0bd85158264e21dbf2080aebd39ea83a96576bd
Mickale Saturre <saturre.mic2@gmail.com>
2019-05-31 12:15:31 +0800

    Created dashboard (static data)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 // Components
 import Navbar from './components/layout/Navbar'
+import Dashboard from './components/dashboard/Dashboard'
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
+
+        {/* Ensures only 1 route is loaded at a time */}
+        <Switch>
+          {/* Dsiplays defined components depending on the route defined */}
+          <Route path="/" component={Dashboard} />
+        </Switch>
       </div>
     </BrowserRouter>
   );

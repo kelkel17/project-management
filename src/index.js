9cc9a1ca1f35f56e24913b9f559a393210d9b320
Mickale Saturre <saturre.mic2@gmail.com>
2019-06-03 15:14:41 +0800

    configuring firebase, firestore to app

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
-import rootReducer from './store/reducers/rootReducer'
+import { reduxFirestore, getFirestore } from 'redux-firestore'
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+// Components
+import rootReducer from './store/reducers/rootReducer'
+import firebaseConfig from './config/firebaseConfig'
+
+const store = createStore(rootReducer,
+	compose(
+		applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+		reduxFirestore(firebaseConfig),
+		reactReduxFirebase(firebaseConfig)
+	)
+);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -9,7 +9,7 @@ const initState = {
 const projectReducer = (state = initState, action) => {
 	switch (action.type) {
 		case 'CREATE_PROJECT':
-			console.log('created project', action.project)
+			console.log('created project', action.project);
 		default:
 			return state;
 	}

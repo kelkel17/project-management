ba1a013d105bb86945fffd36064e1fdcc1d58378
Mickale Saturre <saturre.mic2@gmail.com>
2019-06-03 10:04:33 +0800

    Created Project Details Component (static data)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 // Components
 import Navbar from './components/layout/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
+import ProjectDetails from './components/projects/ProjectDetails'
 
 function App() {
   return (
@@ -13,8 +14,13 @@ function App() {
 
         {/* Ensures only 1 route is loaded at a time */}
         <Switch>
-          {/* Dsiplays defined components depending on the route defined */}
-          <Route path="/" component={Dashboard} />
+          {/*
+            - Dsiplays defined components depending on the route defined
+            - "exact" will load the exact path that has been defined
+          */}
+
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/project/:id" component={ProjectDetails} />
         </Switch>
       </div>
     </BrowserRouter>

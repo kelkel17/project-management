bd117172edba5aed18972d7c311fbdd48670daa7
Mickale Saturre <saturre.mic2@gmail.com>
2019-06-04 10:35:24 +0800

    accessing profile from firebase to app

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -7,9 +7,9 @@ import SignedInLinks from './SignedInLinks'
 import SignedOutLinks from './SignedOutLinks'
 
 const Navbar = (props) => {
-  const { auth } = props;
+  const { auth, profile } = props;
 
-  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
+  const links = auth.uid ? <SignedInLinks profile={profile} /> : <SignedOutLinks />;
 
   return (
     <nav className="nav-wrapper grey darken-3">
@@ -23,7 +23,8 @@ const Navbar = (props) => {
 
 const mapStateToProps = (state) => {
 	return {
-    auth: state.firebase.auth
+    auth: state.firebase.auth,
+    profile: state.firebase.profile
 	}
 }
 
